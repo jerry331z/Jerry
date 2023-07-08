226812a370f6ba9aca64bcd37a699fe069d9a33b
Min Woo Song <hanbyeol332z@daum.net>
2023-07-08 22:24:13 +0900

    Feat. Email validation check
    Check for Email Validation

--- src/main/java/com/example/jerry/user/controller/RestUserController.java
+++ src/main/java/com/example/jerry/user/controller/RestUserController.java
@@ -15,11 +15,15 @@ package com.example.jerry.user.controller;
 
 import com.example.jerry.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.internet.MimeMessage;
 import java.util.HashMap;
+import java.util.Random;
 
 @RestController
 @RequestMapping(value = "/user/*")
@@ -28,10 +32,12 @@ public class RestUserController {
     HashMap<String, Object> data = new HashMap<>();
 
     private final UserService userService;
+    private final JavaMailSender javaMailSender;
 
     @Autowired
-    public RestUserController(UserService userService) {
+    public RestUserController(UserService userService, JavaMailSender javaMailSender) {
         this.userService = userService;
+        this.javaMailSender = javaMailSender;
     }
 
     /* 회원가입 아이디 중복 체크 */
@@ -89,6 +95,46 @@ public class RestUserController {
         } else {
             data.put("result", "success");
         }
+
+        return data;
+    }
+
+    /* 이메일 유효성 검증 */
+    @PostMapping(value = "checkEmail")
+    public HashMap<String, Object> checkEmail(String user_email) {
+
+        Random random = new Random();
+        int checkNum = random.nextInt(888888) + 111111;
+
+        // 메일 제목, 내용
+        String subject = "회원가입 인증 메일입니다.";
+        String content = "홈페이지를 방문해주셔서 감사합니다." +
+                "인증 번호는 " + checkNum + " 입니다." +
+                "\r\n" +
+                "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+
+        // 보내는 사람
+        String from = "hanbyeols333z@gmail.com";
+
+        try {
+            // 메일 내용 넣을 객체와, 이를 도와주는 Helper 객체 생성
+            MimeMessage mail = javaMailSender.createMimeMessage();
+            MimeMessageHelper mailHelper = new MimeMessageHelper(mail, "UTF-8");
+
+            // 메일 내용을 채워줌
+            mailHelper.setFrom(from, "관리자");    // 보내는 사람 셋팅
+            mailHelper.setTo(user_email);        // 받는 사람 셋팅
+            mailHelper.setSubject(subject);    // 제목 셋팅
+            mailHelper.setText(content);    // 내용 셋팅
+
+            // 메일 전송
+            javaMailSender.send(mail);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        data.put("code", checkNum);
 
         return data;
     }
